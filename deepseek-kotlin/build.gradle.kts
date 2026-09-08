12bd31720a299c3cd4004384d3615008c2cbe30b
devcrocod <devcrocod@gmail.com>
2026-09-08 15:53:39 +0200

    fix formatting

--- deepseek-kotlin/build.gradle.kts
+++ deepseek-kotlin/build.gradle.kts
@@ -1,10 +1,10 @@
 @file:OptIn(ExperimentalWasmDsl::class, ExperimentalAbiValidation::class)
 
+import java.time.Year
 import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
-import java.time.Year
 
 plugins {
     alias(libs.plugins.android.kotlin.multiplatform.library)
